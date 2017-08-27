23a6aac0fcddeba3414fb33eedc45aa03a7f8bf0
John Martin <tekbuddha@gmail.com>
2017-08-27 15:22:13 -0700

    moved Terminal & iTerm to indiviual configs

---
+++ conf.d/iterm.sh
@@ -0,0 +1,9 @@
+###############################################################################
+# iTerm 2                                                                     #
+###############################################################################
+
+# Install the Solarized Dark theme for iTerm
+open "${SOURCE}/init/Solarized Dark.itermcolors"
+
+# Don’t display the annoying prompt when quitting iTerm
+defaults write com.googlecode.iterm2 PromptOnQuit -bool false
