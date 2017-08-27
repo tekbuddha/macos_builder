5f5b9a4dd06027864be586dd664c0fec28640c90
John Martin <tekbuddha@gmail.com>
2017-08-27 11:14:13 -0700

    added SOURCE to allow for copying from /init

--- macos-defaults.sh
+++ macos-defaults.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+SOURCE=`pwd`
+
 # Close any open System Preferences panes, to prevent them from overriding
 # settings we’re about to change
 osascript -e 'tell application "System Preferences" to quit'
