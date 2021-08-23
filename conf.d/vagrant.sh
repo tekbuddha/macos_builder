a147b2b756a0cd4d7081f5ba9123386e72109bc0
John Martin <tekbuddha@gmail.com>
2021-08-23 08:07:01 -0700

    updated boxes

--- conf.d/vagrant.sh
+++ conf.d/vagrant.sh
@@ -5,7 +5,7 @@
 
 # This script prefers bento boxes (https://github.com/chef/bento) and the
 # virtualbox provider.
-boxes="centos/7 ubuntu-18.04 ubuntu-20.04"
+boxes="centos/7 bento/ubuntu-18.04 bento/ubuntu-20.04"
 
 for currentBox in $boxes
  do
@@ -13,6 +13,6 @@ for currentBox in $boxes
    then
     echo " ${currentBox} already exists. Skipping..."
   else
-    vagrant box add bento/${currentBox} --provider virtualbox
+    vagrant box add ${currentBox} --provider virtualbox
   fi
  done
