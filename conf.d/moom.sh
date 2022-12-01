7a88e27adc601f29ef668ef35dd708d208dccc7e
John Martin <tekbuddha@gmail.com>
2022-11-30 16:27:15 -0800

    removed moom configs

--- conf.d/moom.sh
+++
@@ -1,8 +0,0 @@
-###############################################################################
-# Moom                                                                        #
-###############################################################################
-
-# Install the preferences file
-cp "${SOURCE}/init/com.manytricks.Moom.plist" ${HOME}/Library/Preferences
-
-killall "Moom" &> /dev/null
