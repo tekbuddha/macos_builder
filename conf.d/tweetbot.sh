80aaab038c4fc658e24fad1138a492938fd71510
John Martin <tekbuddha@gmail.com>
2024-07-03 14:21:41 -0700

    miss you, tweebot.

--- conf.d/tweetbot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-###############################################################################
-# Tweetbot.app                                                                #
-###############################################################################
-
-# Bypass the annoyingly slow t.co URL shortener
-defaults write com.tapbots.TweetbotMac OpenURLsDirectly -bool true
